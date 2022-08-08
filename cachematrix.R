0fa29f9e425a0e48df33f8e9d4e2ff2a669c97df
jcarlingarcia <carlingarc@gmail.com>
2022-08-08 16:37:11 -0500

    edit cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object that stores 
+## a matrix and cache's its inverse. We use the <<- operator to assign a value to
+## an object in an environment that is different from the current environment.
 
-## Write a short comment describing this function
+## The first function creates a special matrix: The first function creates a 
+## special array: set the value of the vector, get the value of the vector, 
+## set the value of the inverse, get the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This second function calculates the inverse of the special matrix created 
+## with the "makeCacheMatrix" function. It first checks if the inverse has 
+## already been computed. If so, it gets the inverse of the cache and skips the 
+## calculation. Otherwise, it calculates the inverse of the matrix and sets the 
+## inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
